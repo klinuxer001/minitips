1cb6e1674603d27d386579abe48c622a5b9988b4
klinuxer001 <jingcao@126.com>
2015-02-28 09:31:44 +0800

    fix getallenv.c

--- snippet/getallenv.c
+++ snippet/getallenv.c
@@ -1,15 +1,34 @@
 #include <stdio.h>
 
 /**
-  *@notice: if '*argp != 0x0' instead of '*argp++ != 0x0', will occure core dump
+  *if *args++!= 0x0 instead of *++args != 0x0, program will core dump, because :
+  *  printf require *args is a NOT-NULL variable.
   */
-
-int main(int argc, char *argv[], char *argp[])
+void dump_env_0(char const *args[])
 {
-	while( *argp != 0x0 )   //NOT  *argp++ != 0x0
+	while( *++args != 0x0 )	//NOT *args++ != 0x0 
 	{
-		printf("%s\n", *argp);
-		argp++;
+		printf("%s\n", *args);
 	}
+}
+
+/**
+   recommand this method, dump_env_0 method 
+   will less 1 line than dump_env_1 method
+**/
+void dump_env_1(char const *args[])		
+{
+	while( *args != 0x0 )
+	{
+		printf("%s\n", *args);
+		args++;
+	}
+}
+
+int main(int argc, char *argv[], char const *argp[])
+{
+//	dump_env_0(argp);
+	dump_env_1(argp);
+
 	return 0;
 }
