b5a80eea4a49d0b6836021fabe45f7e36d781446
klinuxer001 <jingcao@126.com>
2015-03-02 09:54:34 +0800

    add function 'D' and 'c' in parse0183.c

--- snippet/parse0183.c
+++ snippet/parse0183.c
@@ -7,14 +7,20 @@
 #include <stdarg.h>
 #include <string.h>
 
+struct nmea_date {
+	int day;
+	int mon;
+	int year;
+};
+
 /**
   *@param	sentence:输入参数，GPS接收的报文行
   *@param	format:输入参数，具体如下：
 			t:语句类型，表示为某个类型的报文
 			f:输出值类型为浮点数
 			i:输出值类型为整数
-			D:输出值类型为日期型	//TODO:
-			c:输出值类型为字符		//TODO:
+			D:输出值类型为日期型	
+			c:输出值类型为字符	
   
   *@return  0:成功  -1:失败
   */
@@ -68,7 +74,23 @@ int parse_sentence(const char *sentence, char *format, ...)
 				*va_arg(ap, double *) = fv;
 			}
 			break;
+			case 'c':
+			{
+				*va_arg(ap, char *) = *field;
+			}
+			break;
+			case 'D':
+			{
+				struct nmea_date *rcvdate = va_arg(ap, struct nmea_date *);
+				int day = strtol((char []){field[0],field[1],'\0'}, NULL, 10);
+				int mon = strtol((char []){field[2],field[3],'\0'}, NULL, 10);
+				int year = strtol((char []){field[4],field[5],'\0'}, NULL, 10);
 				
+				rcvdate->day = day;
+				rcvdate->mon = mon;
+				rcvdate->year = year;
+			}
+			break;
 			default:
 				result = -1;
 				goto parse_sentence_error;
@@ -87,7 +109,9 @@ int main(int argc, char *argv[])
 	int val = 0;
 	double df = 0.0;
 	char buf[10] = { 0 };
-	parse_sentence("$GPRMC,123,4.5", "tif", buf, &val, &df);
-	printf("%s, %d, %f\n", buf, val, df);
+	char c;
+	struct nmea_date date;
+	parse_sentence("$GPRMC,123,4.5,A,310115", "tifcD", buf, &val, &df, &c, &date);
+	printf("%s, %d, %f, %c, %d, %d, %d\n", buf, val, df, c, date.year, date.mon, date.day);
 	return 0;
 }
