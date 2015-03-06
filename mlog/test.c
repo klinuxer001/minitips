811e7b47ed5ad723813216d1691c09434f77c5d5
klinuxer001 <jingcao@126.com>
2015-03-06 10:35:01 +0800

    add Doxyfile and htmlhead.conf

--- mlog/test.c
+++ mlog/test.c
@@ -1,5 +1,25 @@
+#include <stdio.h>
 #include "mlog.h"
 
+void func3(void)
+{
+	mlog.writeStackTrace();
+}
+
+void func2(void)
+{
+	func3();
+}
+
+void func1(void)
+{
+	func2();
+}
+
+void func(void)
+{
+	func1();
+}
 
 int main(int argc, char *argv[])
 {
@@ -7,5 +27,6 @@ int main(int argc, char *argv[])
 	mlog.writeLog(MLOG_DEBUG, "debug, %d", __LINE__);
 	mlog.writeLog(MLOG_WARN, "warn, %d", __LINE__);
 	mlog.writeLog(MLOG_ERROR, "error, %d", __LINE__);
+	func();
 	return 0;
 }
